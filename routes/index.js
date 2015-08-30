1bd62d1685a4f0e531b13d60b50405aea172b9ec
jbensal <jay.bensal@gmail.com>
2015-08-30 16:08:15 -0500

    adding passport and basic routes

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Founders Resume Book' });
+});
+
+router.get('/students', function(req, res, next){
+	res.render('index', {title: 'Student Login Page' });
+});
+
+router.get('/employers', function(req, res, next){
+	res.render('employer-login', {title: 'Employer Login Page' });
 });
 
 module.exports = router;
